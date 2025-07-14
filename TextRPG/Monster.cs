74c86dc911eabf6d5e2a027ac09818347553fa85
nakyung71 <nakyung71@naver.com>
2025-07-14 11:18:59 +0900

    Update Monster.cs

--- TextRPG/Monster.cs
+++ TextRPG/Monster.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace TextRPG
 {
-    internal class Monster
+    public abstract class Monster
     {
+        public string Name { get; set; }
+        public int Health {  get; set; }
+        public int Attack {  get; set; }
+        public int DropGold { get; set; }
+        public int DropExp { get; set; }
+
+        
+    }
+    public class MInion:Monster
+    {
+        MInion()
+        {
+            Name = "미니언";
+            Health = 0;
+            Attack = 0;
+            DropGold = 0;
+            DropExp= 0;
+        }
+    }
+    public class CannonMinion:Monster
+    {
+        CannonMinion()
+        {
+            Name = "미니언";
+            Health = 0;
+            Attack = 0;
+            DropGold = 0;
+            DropExp = 0;
+        }
+    }
+    public class VoidMinion : Monster
+    {
+        VoidMinion()
+        {
+            Name = "미니언";
+            Health = 0;
+            Attack = 0;
+            DropGold = 0;
+            DropExp = 0;
+        }
     }
 }
