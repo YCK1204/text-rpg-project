d908a121663b894d641a1eb0a6702ede879400eb
YCK1204 <dpcks5959@naver.com>
2025-07-15 17:47:54 +0900

    인벤토리 및 아이템 출력, 사용 기능 추가

--- TextRPG/Player.cs
+++ TextRPG/Player.cs
@@ -4,27 +4,195 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using TextRPG.Utils;
 
 namespace TextRPG
 {
     public class Player
     {
-        public int Level = 1;
-        public string Name = "탐험가";
-        public string Class = "모험가";
-        public int Attack = 11;
-        public int Defense = 12;
-        public int Health = 100;
-        public int MaxHealth = 100;
-        public int Gold = 500;
-        public int Exp = 0;
-        public int NeedExp = 0;
-
-        public Player() 
+        static Player Instance { get; } = new Player();
+        PlayerCharacter _character;
+        public static PlayerCharacter Character 
         {
-            MaxHealth = 100;
-            NeedLevelUPExp();
-        } 
+            get { return Instance._character; }
+            set { Instance._character = value; }
+        }
+        #region Status
+        public static int ID { get { return Character.Id; } }
+        public static int Level 
+        {
+            get
+            {
+                return Character.Status.Level; 
+            }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.Level = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static string Name 
+        {
+            get { return Character.Status.Name; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.Name = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static CharacterClass Class 
+        {
+            get { return Character.Status.Class; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.Class = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int Attack 
+        {
+            get { return Character.Status.Attack; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.Attack = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int Defense 
+        {
+            get { return Character.Status.Defense; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.Defense = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int Health 
+        {
+            get { return Character.Status.Health; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.Health = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int MaxHealth 
+        {
+            get { return Character.Status.MaxHealth; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.MaxHealth = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int Gold 
+        {
+            get { return Character.Status.Gold; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.Gold = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int Exp 
+        {
+            get { return Character.Status.Exp; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.Exp = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int NeedExp 
+        {
+            get { return Character.Status.NeedExp; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.NeedExp = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int ItemAttack 
+        {
+            get { return Character.Status.ItemAttack; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.ItemAttack = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int ItemDefense 
+        {
+            get { return Character.Status.ItemDefense; }
+            set
+            {
+                var playerCharacter = Character;
+                var status = playerCharacter.Status;
+                status.ItemDefense = value;
+                playerCharacter.Status = status;
+                Character = playerCharacter;
+            }
+        }
+        public static int TotalAttack
+        {
+            get { return Attack + ItemAttack; }
+        }
+        public static int TotalDefense
+        {
+            get { return Defense + ItemDefense; }
+        }
+        #endregion
+        public static Inventory Inventory
+        {
+            get { return Character.Inventory; }
+            set
+            {
+                var playerCharacter = Character;
+                playerCharacter.Inventory = value;
+                Character = playerCharacter;
+            }
+        }
+        public static void UnEquipItemArmor()
+        {
+            Inventory.UnEquipItemArmor();
+        }
+        public static void UnEquipItemWeapon()
+        {
+            Inventory.UnEquipItemWeapon();
+        }
         public void playerinfo()
         {
             Console.WriteLine($"Level: {Level}");
@@ -38,23 +206,5 @@ namespace TextRPG
             Console.WriteLine("아무키나 입력시 시작화면으로 돌아갑니다.");
             Console.ReadKey();
         }
-        private void LevelUP() // 레벨업 시 능력치를 증가시키는 메서드
-        {
-            Level++; // 레벨업
-            Attack += 2; // 공격력 2 증가
-            Defense += 3; // 방어력 3씩 증가
-            MaxHealth += 20; // 최대 체력 20씩 증가
-            Health = MaxHealth; // 레벨업 시 체력이 최대치로 회복됨
-            NeedLevelUPExp();
-        }
-        private void NeedLevelUPExp()
-        {
-            NeedExp = Level * 500;
-        }
-        
-
-
-        
-
     }
 }
