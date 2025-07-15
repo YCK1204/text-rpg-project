3cd5696a16ae4f864257c377164725070f059f72
anvilking1500 <vhffltmgur@nate.com>
2025-07-15 16:52:41 +0900

    Update GameManager.cs

--- TextRPG/GameManager.cs
+++ TextRPG/GameManager.cs
@@ -31,9 +31,9 @@ namespace TextRPG
             Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
             Console.WriteLine();
             Console.WriteLine("1. 상태 보기"); // 1 입력시 Player 스크립트에 입력된 상태 호출
-            Console.WriteLine("2. 인벤토리 보기");
-            Console.WriteLine("3. 전투 시작"); // 전투 화면을 불러옴
-            Console.WriteLine("4. 게임 종료");
+            Console.WriteLine("2. 인벤토리 보기"); // 2 입력시 인벤토리 화면을 불러옴
+            Console.WriteLine("3. 전투 시작"); // 3 입력시 전투 시작 화면을 불러옴
+            Console.WriteLine("4. 게임 종료");// 4 입력시 게임을 종료시킴
             Console.WriteLine();
             Console.Write("원하시는 행동을 입력해 주세요:"); // 1~3까지 숫자 입력 시 맞는 화면이 출력됨
             string input = Console.ReadLine(); //유저가 번호를 입력하는 곳
@@ -42,12 +42,15 @@ namespace TextRPG
             {
                 case "1":
                     player.playerinfo(); // 플레이어의 정보를 보여주는 화면
+                    // 현재 플레이어의 레벨 이름 능력치, 경험치 등을 보여준다.
                     break;
                 case "2":
                     break;
                     //플레이어의 인벤토리를 보여줍니다.
+                    //인벤토리는 아직 완성되지 않음
                 case "3":
                     List<dynamic> list = new List<dynamic>(); // 3번 입력시 전투 화면을 불러옴 플레이어vs몬스터
+                    // 전투 시작 화면을 알려주세요!
                     break;
                 case "4":
                     Console.WriteLine("게임을 종료합니다.");
