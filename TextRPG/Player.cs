68c8b7739885b757b4fbfd8ef4b510e7e0a7dd41
nakyung71 <nakyung71@naver.com>
2025-07-18 20:49:39 +0900

    렙업로직 최종수정

--- TextRPG/Player.cs
+++ TextRPG/Player.cs
@@ -143,6 +143,7 @@ namespace TextRPG
                 Level += 1;
                 Console.WriteLine($"레벨업! 플레이어 레벨이{Level} 이 되었습니다");
                 Console.ReadKey();
+                neededExp = Level * Level * 100;
             }
 
 
